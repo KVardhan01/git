76f395eea1a49e1d2c00949e4713a46e59dc8d7c
vardhan <kvardhan01@gmail.com>
2024-06-03 16:59:45 +0530

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FCode_CRUD.Controllers
+{
+    public class HomeController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+
+            return Ok("vardhan");
+        }
+    }
+}
