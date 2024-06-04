5445e2e8a096303997010c6c3954e97c5108d735
vardhan <kvardhan01@gmail.com>
2024-06-04 12:29:23 +0530

    tasdl

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace FCode_CRUD.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "gjhWarm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "gjhWarm", "Balmy", "Hot", "Sweltetasskring", "Scorching"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
